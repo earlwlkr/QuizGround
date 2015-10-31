e2b70803f80378774bd9260b254aab3538bbc96e
earlwlkr <mike358015@gmail.com>
2015-10-31 13:22:47 +0700

    Update to OAuth2 password grant

--- backend/models/access-token.js
+++ backend/models/access-token.js
@@ -4,7 +4,8 @@ var Schema = mongoose.Schema;
 var schema = new Schema({
     userId:         String,
     clientId:       String,
-    accessToken:    String
+    accessToken:    String,
+    expireDate:     Date
 });
 
 module.exports = mongoose.model('AccessToken', schema);
