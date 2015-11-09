3979279cb2bf60a81e58c0e7861883f122c1ca75
earlwlkr <mike358015@gmail.com>
2015-11-09 21:34:54 +0700

    Accept Authorization headers

--- backend/app.js
+++ backend/app.js
@@ -19,12 +19,15 @@ app.use(passport.session());
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
 });
 
 mongoose.connect('mongodb://localhost/quizground');
-
 
 
 app.post('/oauth2/token', oauth2.token);
